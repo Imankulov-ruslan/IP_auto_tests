86293435f71a11e42c9365724193e5a644c37705
Imankulov-ruslan <75061539+Imankulov-ruslan@users.noreply.github.com>
2023-06-16 19:58:15 +0300

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,2 @@
 
-robot $args --exclude $exclude_tags  --variable ui_auth_password:$ui_auth_password --variable api_password:$api_password --variable db_password:$db_password -A src/config/test-runner/$ENV-args.robot ||
-(robot --rerunfailed reports/output.xml --output rerun.xml $args --exclude $exclude_tags  --variable ui_auth_password:$ui_auth_password --variable api_password:$api_password --variable db_password:$db_password -A src/config/test-runner/$ENV-args.robot;
-rebot --merge -d reports reports/output.xml reports/rerun.xml)
+robot $args --exclude $exclude_tags  --variable ui_auth_password:$ui_auth_password -A src/config/test-runner/dev-args.robot ||
